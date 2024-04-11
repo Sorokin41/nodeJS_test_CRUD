9d283a7fb2261306176060eb4543021400a594c4
Vladislav <60213212@leroymerlin.ru>
2024-04-11 17:36:47 +0300

    change pool and docker compose

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'user',
-    password: 'password',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     host: 'chatops-db',
     port: 5432,
-    database: 'debug'
+    database: process.env.POSTGRES_DB
 });
 
 module.exports = pool;
