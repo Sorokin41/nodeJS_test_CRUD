e2810e1893311beb7771906b38f11b1afa4b1cbb
Vladislav <60213212@leroymerlin.ru>
2024-05-02 17:54:48 +0300

    xx

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express = require('express')
 require('dotenv').config()
-const pool = require('./src/db/pool')
+const userRouter = require('./src/routes/user.routes')
 
 const app = express()
 
-app.get('/userdata', async (req, res) => {
-    try {
-        const newUser = await pool.query(
-            `SELECT * FROM public.newtable`
-        )
-        res.json(newUser)
-    } catch (err) {
-        console.error(err);
-    }
-})
+app.use('/', userRouter)
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server was started on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => console.log(`Server was started on port ${process.env.PORT}`))
--- src/db/pool.js
+++ src/db/pool.js
@@ -3,9 +3,19 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    host: 'chatops-db',
-    port: 5432,
+    host: 'localhost',
+    port: 6432,
     database: process.env.POSTGRES_DB
 });
+
+pool.query(`
+    create TABLE IF NOT EXISTS person(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255));
+`).then(() => {
+    console.log('Table is successfully created or already exists');
+    pool.end()
+})
 
 module.exports = pool;
