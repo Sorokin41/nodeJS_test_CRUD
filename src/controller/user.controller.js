14447afc5ab12e3456fa07d232a539dcc4eda9a6
Vladislav <60213212@leroymerlin.ru>
2024-05-07 16:15:51 +0300

    metrics fix

---
+++ src/controller/user.controller.js
@@ -0,0 +1,41 @@
+const db = require('../db/pool')
+const { incrementHttpRequestMethod, responseTimeMiddleware } = require('../metrics/metrics')
+
+class UserController{
+
+    async createUser(req, res) {
+        incrementHttpRequestMethod(req.method)
+        const {name, surname} = req.body
+        const newPerson = await db.query('INSERT INTO person (name, surname) values ($1, $2) RETURNING *', [name, surname])
+        res.json(newPerson.rows[0])
+    }
+
+    async getUsers(req, res) {
+        incrementHttpRequestMethod(req.method)
+        const users = await db.query('SELECT * FROM person')
+        res.json(users.rows)
+    }
+
+    async getUser(req, res) {
+        incrementHttpRequestMethod(req.method)
+        const id = req.params.id
+        const user = await db.query('SELECT * FROM person WHERE id = $1', [id])
+        res.json(user.rows[0])
+    }
+
+    async updateUser(req, res) {
+        incrementHttpRequestMethod(req.method)
+        const {id, name, surname} = req.params.id
+        const newUser = await db.query('UPDATE person SET name = $2, surname = $3 WHERE id = $1', [id, name, surname])
+        res.json(newUser.rows[0])
+    }
+
+    async deleteUser(req, res) {
+        incrementHttpRequestMethod(req.method)
+        const id = req.params.id
+        const user = await db.query('DELETE FROM person WHERE id = $1', [id])
+        res.json(user.rows[0])
+    }
+}
+
+module.exports = new UserController()
