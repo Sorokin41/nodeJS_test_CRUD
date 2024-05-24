5acdb936a404cb2d46a74bb432fb5e32642a996b
Vladislav <60213212@leroymerlin.ru>
2024-05-24 13:49:03 +0300

    dix

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
+const pino = require('pino');
+const pinoHttp = require('pino-http');
 require('dotenv').config()
-const client = require('prom-client')
 
 const userRouter = require('./src/routes/user.routes')
-const { responseTimeMiddleware, metricsMiddleware } = require('./src/metrics/metrics')
+const { metricsMiddleware } = require('./src/metrics/metrics')
+
+const logDirectory = path.join(__dirname, 'logs')
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
+// Настройка потока записи логов в файл
+const logStream = fs.createWriteStream(path.join(logDirectory, 'app.log'), { flags: 'a' });
+
+// Настройка Pino для записи логов в файл
+const logger = pino(pino.destination(logStream));
 
 const app = express()
 app.use(express.json())
 app.use('/', userRouter)
+
+const child = logger.child({ a: 'property' })
+
+// Настройка Pino для логирования HTTP запросов
+app.use(pinoHttp({ logger }));
 
 app.use((req, res, next) => {
     next()
@@ -15,4 +35,7 @@ app.use((req, res, next) => {
 
 app.get('/metrics', metricsMiddleware);
 
-app.listen(process.env.PORT, () => console.log(`Server was started on port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => {
+  child.info('qq')
+  console.log(`Server was started on port ${process.env.PORT}`)
+})
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,6 @@
 const db = require('../db/pool')
-const { incrementHttpRequestMethod, responseTimeMiddleware } = require('../metrics/metrics')
+const { incrementHttpRequestMethod } = require('../metrics/metrics')
+const logger = require('../logger/logger')
 
 class UserController{
 
@@ -7,12 +8,16 @@ class UserController{
         incrementHttpRequestMethod(req.method, res.statusCode)
         const {name, surname} = req.body
         const newPerson = await db.query('INSERT INTO person (name, surname) values ($1, $2) RETURNING *', [name, surname])
+        logger.info(`url: ${req.url}, status: ${res.statusCode}, method:${req.method}`)
+        logger.info(newPerson.rows[0])
         res.json(newPerson.rows[0])
     }
 
     async getUsers(req, res) {
         incrementHttpRequestMethod(req.method, res.statusCode)
         const users = await db.query('SELECT * FROM person')
+        logger.info(`url: ${req.url}, status: ${res.statusCode}, method:${req.method}`)
+        logger.info(users.rows)
         res.json(users.rows)
     }
 
@@ -20,6 +25,7 @@ class UserController{
         incrementHttpRequestMethod(req.method, res.statusCode)
         const id = req.params.id
         const user = await db.query('SELECT * FROM person WHERE id = $1', [id])
+        logger.info(user.rows[0])
         res.json(user.rows[0])
     }
 
